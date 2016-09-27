2951d4897d88f6c4e399e3dafa7c36f783b67c51
Filipe Toscano <ft@itm.net>
2016-09-28 00:10:13 +0200

    + Add: Generate the Nuget for Zinc Transforms, replacing the version in build time. + Add: Project for Zinc web extensions -- which will mostly be for generation of description formats (WADL, RAML, Swagger).

---
+++ src/Zinc.Web/WadlGeneratorHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+
+namespace Zinc.Web
+{
+    /// <summary />
+    public class WadlGeneratorHandler : IHttpHandler
+    {
+        /// <summary />
+        public void ProcessRequest( HttpContext context )
+        {
+            context.Response.ContentType = "application/xml";
+            context.Response.Write( "<root />" );
+        }
+
+
+        /// <summary />
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+    }
+}
