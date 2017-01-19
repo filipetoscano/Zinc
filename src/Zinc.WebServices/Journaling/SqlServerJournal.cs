b4e85a4364c22c615b2abc695f7c6129d7ec6c69
Filipe Toscano <ft@itm.net>
2017-01-19 10:36:43 +0100

    * Change: Bump to Platinum 1.0.1

--- src/Zinc.WebServices/Journaling/SqlServerJournal.cs
+++ src/Zinc.WebServices/Journaling/SqlServerJournal.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Dapper;
+using Platinum.Data;
+using System;
+using System.Data.Common;
 using System.Threading.Tasks;
 
 namespace Zinc.WebServices.Journaling
@@ -9,6 +12,25 @@ namespace Zinc.WebServices.Journaling
         /// <summary />
         public Task PreAsync( ExecutionContext context, object request )
         {
+            #region Validations
+
+            if ( context == null )
+                throw new ArgumentNullException( nameof( context ) );
+
+            if ( request == null )
+                throw new ArgumentNullException( nameof( request ) );
+
+            #endregion
+
+
+            /*
+             * 
+             */
+            DbConnection conn = Db.Connection( "SqlServerLogging" );
+
+
+
+
             // TODO:
             return Task.CompletedTask;
         }
@@ -17,6 +39,17 @@ namespace Zinc.WebServices.Journaling
         /// <summary />
         public Task PostAsync( ExecutionContext context, object response )
         {
+            #region Validations
+
+            if ( context == null )
+                throw new ArgumentNullException( nameof( context ) );
+
+            if ( response == null )
+                throw new ArgumentNullException( nameof( response ) );
+
+            #endregion
+
+
             // TODO:
             return Task.CompletedTask;
         }
@@ -25,6 +58,20 @@ namespace Zinc.WebServices.Journaling
         /// <summary />
         public Task FullAsync( ExecutionContext context, object request, object response )
         {
+            #region Validations
+
+            if ( context == null )
+                throw new ArgumentNullException( nameof( context ) );
+
+            if ( request == null )
+                throw new ArgumentNullException( nameof( request ) );
+
+            if ( response == null )
+                throw new ArgumentNullException( nameof( response ) );
+
+            #endregion
+
+
             // TODO:
             return Task.CompletedTask;
         }
