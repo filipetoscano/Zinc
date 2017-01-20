5a19e4f0ce79435c274142abe89a7f9731d4d609
Filipe Toscano <ft@itm.net>
2017-01-20 11:51:00 +0100

    * Change: Put the transforms into a seperate project, just for cleanup sake.

---
+++ src/Zinc.Transforms/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General information
+[assembly: AssemblyTitle( "Zinc - Transforms" )]
+[assembly: AssemblyDescription( "Dummy project to hold transforms - not actually compiled :-)" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "Zinc" )]
+[assembly: AssemblyCopyright( "" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Additional information
+[assembly: ComVisible( false )]
+[assembly: Guid( "3c73c964-278f-4bc1-876d-f8467f5522fc" )]
+
+// Version information
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
