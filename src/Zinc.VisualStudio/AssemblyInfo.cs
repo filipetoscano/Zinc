8cbe8f5cd631ae47c638ff22429b71af60fa18bb
Filipe Toscano <ft@itm.net>
2017-02-18 02:19:28 +0100

    + Add: Zinc Visual Studio plugin, to boost productivity.

---
+++ src/Zinc.VisualStudio/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitle( "Zinc Visual Studio" )]
+[assembly: AssemblyDescription( "Code generators for Visual Studio." )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Transition" )]
+[assembly: AssemblyProduct( "Zinc" )]
+[assembly: AssemblyCopyright( "" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+[assembly: AssemblyDelaySign( false )]
+[assembly: NeutralResourcesLanguage( "en-US" )]
+
+[assembly: AssemblyVersion( "1.0.0.0" )]
---
+++ src/Zinc.VisualStudio/ZnTool.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Platinum.VisualStudio;
+using System;
+using System.Runtime.InteropServices;
+using VSLangProj80;
+
+namespace Zinc.VisualStudio
+{
+    /// <summary>
+    /// Generate configuration section handler.
+    /// </summary>
+    [ComVisible( true )]
+    [Guid( "f2b83cff-e664-472b-800d-09ad50d00a82" )]
+    [CodeGeneratorRegistration( typeof( ZnTool ), "Test Tool", vsContextGuids.vsContextGuidVCSProject, GeneratesDesignTimeSource = true )]
+    [ProvideObject( typeof( ZnTool ) )]
+    public class ZnTool : BaseTool
+    {
+        /// <summary>
+        /// Executes tool.
+        /// </summary>
+        protected override string Execute( string inputNamespace, string inputFileName, string inputContent, bool whatIf )
+        {
+            #region Validations
+
+            if ( inputNamespace == null )
+                throw new ArgumentNullException( nameof( inputNamespace ) );
+
+            if ( inputFileName == null )
+                throw new ArgumentNullException( nameof( inputFileName ) );
+
+            if ( inputContent == null )
+                throw new ArgumentNullException( nameof( inputContent ) );
+
+            #endregion
+
+            return "// ZnTool";
+        }
+    }
+}
