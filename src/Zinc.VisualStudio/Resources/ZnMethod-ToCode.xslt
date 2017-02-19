036beea324010f1d2bebf02c0cdf9756bf4817af
Filipe Toscano <ft@itm.net>
2017-02-19 11:43:17 +0100

    ~ Temporary commit. Will update Zinc plugin to match new structure from Platinum.

---
+++ src/Zinc.VisualStudio/Resources/ZnMethod-ToCode.xslt
@@ -0,0 +1,47 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    xmlns:msxsl="urn:schemas-microsoft-com:xslt"
+    xmlns:zn="urn:zinc"
+    xmlns:fn="urn:eo-util"
+    exclude-result-prefixes="msxsl zn fn">
+
+    <xsl:output method="text" indent="no" />
+
+    <xsl:param name="ToolVersion" />
+    <xsl:param name="FileName" />
+    <xsl:param name="FullFileName" />
+    <xsl:param name="Namespace" />
+
+
+    <!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ~
+    ~ zn:method/
+    ~
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+    <xsl:template match=" zn:method ">
+        <xsl:text>using System;
+using System.Threading.Tasks;
+
+namespace </xsl:text>
+        <xsl:value-of select=" $Namespace " />
+        <xsl:text>
+{
+    public partial class </xsl:text>
+        <xsl:value-of select=" $FileName " />
+        <xsl:text>Implementation
+    {
+        /// &lt;summary /&gt;
+        public Task&lt;</xsl:text>
+        <xsl:value-of select=" $FileName " />
+        <xsl:text>Response&gt; InnerRun( </xsl:text>
+        <xsl:value-of select=" $FileName " />
+        <xsl:text>Request request )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+</xsl:text>
+    </xsl:template>
+
+</xsl:stylesheet>
--- src/Zinc.VisualStudio/ZnTool.cs
+++ src/Zinc.VisualStudio/ZnTool.cs
@@ -1,17 +1,20 @@
 ﻿using Microsoft.VisualStudio.Shell;
 using Platinum.VisualStudio;
 using System;
+using System.IO;
 using System.Runtime.InteropServices;
+using System.Xml;
 using VSLangProj80;
 
 namespace Zinc.VisualStudio
 {
     /// <summary>
-    /// Generate configuration section handler.
+    /// Generate code-behind for Services / Methods. For Methods, also adds
+    /// initial (empty) implementation for Method -- adding file to .csproj!
     /// </summary>
     [ComVisible( true )]
     [Guid( "f2b83cff-e664-472b-800d-09ad50d00a82" )]
-    [CodeGeneratorRegistration( typeof( ZnTool ), "Test Tool", vsContextGuids.vsContextGuidVCSProject, GeneratesDesignTimeSource = true )]
+    [CodeGeneratorRegistration( typeof( ZnTool ), "Zn Tool", vsContextGuids.vsContextGuidVCSProject, GeneratesDesignTimeSource = true )]
     [ProvideObject( typeof( ZnTool ) )]
     public class ZnTool : BaseTool
     {
@@ -33,7 +36,94 @@ namespace Zinc.VisualStudio
 
             #endregion
 
-            return "// ZnTool";
+
+            /*
+             * 
+             */
+            ToolGenerateArgs args = new ToolGenerateArgs()
+            {
+                Content = inputContent,
+                FileName = inputFileName,
+                Namespace = inputNamespace,
+                WhatIf = whatIf,
+            };
+
+            IToolChain xsltTool = new PtXsltTool();
+            string outputContent = xsltTool.Generate( args );
+
+
+            /*
+             * 
+             */
+            XmlNamespaceManager manager = new XmlNamespaceManager( new NameTable() );
+            manager.AddNamespace( "zn", "urn:zinc" );
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml( inputContent );
+
+            if ( doc.DocumentElement.NamespaceURI == "urn:zinc" 
+                 && doc.DocumentElement.LocalName == "method" )
+            {
+                string impl = Path.GetFileNameWithoutExtension( inputFileName ) + "Impl.cs";
+
+                if ( File.Exists( impl ) == false )
+                {
+                    XmlElement notImpl = (XmlElement) doc.SelectSingleNode( " /zn:method/zn:notImplemented " );
+
+                    if ( notImpl == null )
+                    {
+                        GenerateAddImpl( args );
+                    }
+                }
+            }
+
+
+            /*
+             * 
+             */
+            return outputContent;
+        }
+
+
+        private void GenerateAddImpl( ToolGenerateArgs args )
+        {
+            /*
+
+    <Compile Include="OneService\MethodOne.cs">
+      <AutoGen>True</AutoGen>
+      <DesignTime>True</DesignTime>
+      <DependentUpon>MethodOne.xml</DependentUpon>
+    </Compile>
+    <Compile Include="OneService\MethodOneImpl.cs" />
+
+             */
+
+
+            /*
+             * 
+             */
+            FileInfo file = new FileInfo( args.FileName );
+
+            FileInfo[] projs = file.Directory.Parent.GetFiles( "*.csproj" );
+
+            if ( projs == null || projs.Length == 0 )
+                throw new ToolException( "No .csproj found in parent directory from zn:method." );
+
+            if ( projs.Length != 1 )
+                throw new ToolException( "More than one .csproj found in parent directory from zn:method." );
+
+            string csproj = projs[ 0 ].FullName;
+            Console.WriteLine( csproj );
+
+
+            /*
+             * 
+             */
+            string find = file.DirectoryName + "\\" + Path.GetFileNameWithoutExtension( file.Name ) + ".cs";
+            string addf = file.DirectoryName + "\\" + Path.GetFileNameWithoutExtension( file.Name ) + "Impl.cs";
+
+            Console.WriteLine( find );
+            Console.WriteLine( addf );
         }
     }
 }
