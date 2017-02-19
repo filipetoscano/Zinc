88e5ab364f4d308848a7fa6b8161bc1cc9351930
Filipe Toscano <ft@itm.net>
2017-02-19 17:04:57 +0100

    * Change: Rename tool to ZnTool, where applicable. + Add: Fully implement ZnTool: now adds the Impl.cs class to the project! :)

---
+++ src/Zinc.VisualStudio/X.cs
@@ -0,0 +1,76 @@
+﻿using Platinum.VisualStudio;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+
+namespace Zinc.VisualStudio
+{
+    /// <summary />
+    internal class X
+    {
+        /// <summary />
+        internal static XslCompiledTransform LoadXslt( string name )
+        {
+            #region Validations
+
+            if ( name == null )
+                throw new ArgumentNullException( nameof( name ) );
+
+            #endregion
+
+            string fullName = "Zinc.VisualStudio.Resources." + name;
+
+            XsltSettings settings = new XsltSettings( true, true );
+            XmlResolver resolver = new XmlUrlResolver();
+            XslCompiledTransform xsl = new XslCompiledTransform();
+
+            Stream xsltStream = typeof( X ).Assembly.GetManifestResourceStream( fullName );
+
+            if ( xsltStream == null )
+                throw new ToolException( $"Resource file '{ fullName }' not found." );
+
+            using ( XmlReader xr = XmlReader.Create( xsltStream ) )
+            {
+                xsl.Load( xr, settings, resolver );
+            }
+
+            return xsl;
+        }
+
+
+        /// <summary />
+        internal static string ToText( XslCompiledTransform xslt, XsltArgumentList args, XmlDocument xdoc )
+        {
+            #region Validations
+
+            if ( xslt == null )
+                throw new ArgumentNullException( nameof( xslt ) );
+
+            if ( xdoc == null )
+                throw new ArgumentNullException( nameof( xdoc ) );
+
+            #endregion
+
+            XPathNavigator xpNav = xdoc.DocumentElement.CreateNavigator();
+            StringBuilder sb = new StringBuilder();
+
+            using ( TextWriter writer = new StringWriter( sb, CultureInfo.InvariantCulture ) )
+            {
+                try
+                {
+                    xslt.Transform( xpNav, args, writer );
+                }
+                catch ( Exception ex )
+                {
+                    throw new ToolException( "Failed to apply XSLT transformation", ex );
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
--- src/Zinc.VisualStudio/ZnTool.cs
+++ src/Zinc.VisualStudio/ZnTool.cs
@@ -1,7 +1,9 @@
 ﻿using Platinum.VisualStudio;
 using System;
 using System.IO;
+using System.Reflection;
 using System.Xml;
+using System.Xml.Xsl;
 
 namespace Zinc.VisualStudio
 {
@@ -15,34 +17,14 @@ namespace Zinc.VisualStudio
             /*
              * #1. Apply XSLT
              */
-            IGenerator xsltTool = new XsltTool();
+            IGenerator xsltTool = new PtXsltTool();
             string outputContent = xsltTool.Generate( args );
 
 
             /*
-             * #2. Check if need to generate Impl.cs
+             * #2. Conditionally generate $MethodImpl.cs skeleton file.
              */
-            XmlNamespaceManager manager = new XmlNamespaceManager( new NameTable() );
-            manager.AddNamespace( "zn", "urn:zinc" );
-
-            XmlDocument doc = new XmlDocument();
-            doc.LoadXml( args.Content );
-
-            if ( doc.DocumentElement.NamespaceURI == "urn:zinc"
-                 && doc.DocumentElement.LocalName == "method" )
-            {
-                string impl = Path.GetFileNameWithoutExtension( args.FileName ) + "Impl.cs";
-
-                if ( File.Exists( impl ) == false )
-                {
-                    XmlElement notImpl = (XmlElement) doc.SelectSingleNode( " /zn:method/zn:notImplemented " );
-
-                    if ( notImpl == null )
-                    {
-                        GenerateAddImpl( args );
-                    }
-                }
-            }
+            MethodAddImpl( args );
 
 
             /*
@@ -52,22 +34,93 @@ namespace Zinc.VisualStudio
         }
 
 
-        private void GenerateAddImpl( ToolGenerateArgs args )
+        /// <summary>
+        /// Generates a bare skeleton method implementation class.
+        /// </summary>
+        /// <param name="args">Generation arguments.</param>
+        private static void MethodAddImpl( ToolGenerateArgs args )
         {
             /*
-
-    <Compile Include="OneService\MethodOne.cs">
-      <AutoGen>True</AutoGen>
-      <DesignTime>True</DesignTime>
-      <DependentUpon>MethodOne.xml</DependentUpon>
-    </Compile>
-    <Compile Include="OneService\MethodOneImpl.cs" />
-
+             * 
              */
+            XmlNamespaceManager manager = new XmlNamespaceManager( new NameTable() );
+            manager.AddNamespace( "zn", "urn:zinc" );
+            manager.AddNamespace( "vs", "http://schemas.microsoft.com/developer/msbuild/2003" );
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml( args.Content );
+
+
+            /*
+             * Is method?
+             */
+            if ( doc.DocumentElement.NamespaceURI != "urn:zinc" || doc.DocumentElement.LocalName != "method" )
+                return;
+
+
+            /*
+             * Already has implementation file?
+             */
+            string impl = Path.Combine(
+                    Path.GetDirectoryName( args.FileName ),
+                    Path.GetFileNameWithoutExtension( args.FileName ) + "Impl.cs" );
+
+            Console.WriteLine( impl );
+            if ( File.Exists( impl ) == true )
+                return;
+
+
+            /*
+             * Is it marked as zn:notImplemented?
+             */
+            XmlElement notImpl = (XmlElement) doc.SelectSingleNode( " /zn:method/zn:notImplemented ", manager );
+
+            if ( notImpl != null )
+                return;
+
+
+            /*
+             * Generate Impl.cs
+             */
+            var xslt = X.LoadXslt( "ZnMethod-ToCode.xslt" );
 
 
             /*
              * 
+             */
+            FileInfo inputFile = new FileInfo( args.FileName );
+
+            Uri fileUri = new Uri( inputFile.FullName );
+            Uri directoryUri = new Uri( inputFile.DirectoryName );
+            string rawName = inputFile.Name.Substring( 0, inputFile.Name.Length - inputFile.Extension.Length );
+
+            XsltArgumentList xsltArgs = new XsltArgumentList();
+            xsltArgs.AddParam( "ToolVersion", "", Assembly.GetExecutingAssembly().GetName( false ).Version.ToString( 4 ) );
+            xsltArgs.AddParam( "FileName", "", rawName );
+            xsltArgs.AddParam( "FullFileName", "", inputFile.FullName );
+            xsltArgs.AddParam( "UriFileName", "", fileUri.AbsoluteUri );
+            xsltArgs.AddParam( "UriDirectory", "", directoryUri.AbsoluteUri );
+            xsltArgs.AddParam( "Namespace", "", args.Namespace );
+
+            xsltArgs.AddExtensionObject( "urn:eo-util", new XsltExtensionObject() );
+
+            string implContent = X.ToText( xslt, xsltArgs, doc );
+
+            if ( args.WhatIf == false )
+            {
+                try
+                {
+                    File.WriteAllText( impl, implContent );
+                }
+                catch ( Exception ex )
+                {
+                    throw new ToolException( $"Failed to write skeleton implementation to '{ impl }'.", ex );
+                }
+            }
+
+
+            /*
+             * Modify the .csproj
              */
             FileInfo file = new FileInfo( args.FileName );
 
@@ -80,17 +133,51 @@ namespace Zinc.VisualStudio
                 throw new ToolException( "More than one .csproj found in parent directory from zn:method." );
 
             string csproj = projs[ 0 ].FullName;
-            Console.WriteLine( csproj );
+
+            XmlDocument projDoc = new XmlDocument();
+            projDoc.Load( csproj );
 
 
             /*
              * 
-             */
-            string find = file.DirectoryName + "\\" + Path.GetFileNameWithoutExtension( file.Name ) + ".cs";
-            string addf = file.DirectoryName + "\\" + Path.GetFileNameWithoutExtension( file.Name ) + "Impl.cs";
 
-            Console.WriteLine( find );
-            Console.WriteLine( addf );
+    <Compile Include="OneService\MethodOne.cs">
+      <AutoGen>True</AutoGen>
+      <DesignTime>True</DesignTime>
+      <DependentUpon>MethodOne.xml</DependentUpon>
+    </Compile>
+    <Compile Include="OneService\MethodOneImpl.cs" />
+
+             * 
+             */
+            string find = file.Directory.Name + "\\" + Path.GetFileNameWithoutExtension( file.Name ) + ".cs";
+            string addf = file.Directory.Name + "\\" + Path.GetFileNameWithoutExtension( file.Name ) + "Impl.cs";
+
+            XmlElement findElem = (XmlElement) projDoc.SelectSingleNode( $" /vs:Project//vs:Compile[ @Include = '{ find }' ] ", manager );
+            XmlElement addfElem = (XmlElement) projDoc.SelectSingleNode( $" /vs:Project//vs:Compile[ @Include = '{ addf }' ] ", manager );
+
+            if ( findElem != null && addfElem == null )
+            {
+                addfElem = projDoc.CreateElement( "Compile", "http://schemas.microsoft.com/developer/msbuild/2003" );
+
+                var include = projDoc.CreateAttribute( "Include" );
+                include.Value = addf;
+                addfElem.Attributes.Append( include );
+
+                findElem.ParentNode.InsertAfter( addfElem, findElem );
+
+                if ( args.WhatIf == false )
+                {
+                    try
+                    {
+                        projDoc.Save( csproj );
+                    }
+                    catch ( Exception ex )
+                    {
+                        throw new ToolException( "Failed overwriting project file.", ex );
+                    }
+                }
+            }
         }
     }
 }
