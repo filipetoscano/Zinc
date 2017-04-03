d2fdce0da4af85cf7a893aadce717bf183350132
Filipe Toscano <filipe.toscano@gmail.com>
2017-04-03 13:09:07 +0200

    Seperate solution for testing of client proxy

---
+++ tests/Zinc.WebServices.RestClient/ClientTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Zn.Sample;
+
+namespace Zinc.WebServices.RestClient.Test
+{
+    [TestClass]
+    public class ClientTests
+    {
+        [TestMethod]
+        public void MethodOneAsync1()
+        {
+            OneClient svc = new OneClient();
+
+            var response = svc.MethodOneAsync( new Zn.Sample.OneSvc.MethodOneRequest()
+            {
+                Value = 10
+            } ).GetAwaiter().GetResult();
+
+            Assert.AreEqual( 100, response.Value );
+        }
+
+
+        [TestMethod]
+        public void MethodOneAsync2()
+        {
+            OneClient svc = new OneClient();
+
+            try
+            {
+                svc.MethodOneAsync( new Zn.Sample.OneSvc.MethodOneRequest()
+                {
+                    Value = 15
+                } ).GetAwaiter().GetResult();
+
+                Assert.Fail( "Expected exception." );
+            }
+            catch ( ServiceFaultException ex )
+            {
+                // Expecting: Unhandled exception
+                Assert.AreEqual( "ZnSample.Zinc", ex.Actor );
+                Assert.AreEqual( 4006, ex.Code );
+            }
+            catch ( Exception ex )
+            {
+                Assert.Fail( "Expected named exception: " + ex.GetType().FullName );
+            }
+        }
+    }
+}
---
+++ tests/Zinc.WebServices.RestClient/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General information
+[assembly: AssemblyTitle( "Zinc.WebServices.RestClient - Tests" )]
+[assembly: AssemblyDescription( "Unit tests for Zinc.WebServices.RestClient" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Transition" )]
+[assembly: AssemblyProduct( "Zinc" )]
+[assembly: AssemblyCopyright( "" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Additional information
+[assembly: ComVisible( false )]
+[assembly: Guid( "c50433e7-a31f-4b67-9f19-bf6bafa620e2" )]
+
+// Version information
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
